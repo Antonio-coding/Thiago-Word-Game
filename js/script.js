0a07d44359c2df65988cd0ba7242f17c371411e7
Antônio Thiago Neto <112974950+Antonio-coding@users.noreply.github.com>
2023-03-18 22:58:39 -0300

    primeira versão
    concluída

--- js/script.js
+++ js/script.js
@@ -8,17 +8,33 @@ const jump = () => {
     mario.classList.add('jump');
 
     setTimeout(() => {
-
         mario.classList.remove('jump');
-
     }, 500);
 }
 
-const loop= setInterval(()=>{
-    const pipePosition =  pipe.offsetLeft;
+const loop= setInterval(() => {
+console.log ('loop')
 
-    if (pipePosition)
-    console.log ('pipePosition')
-},10);
+    const pipePosition =  pipe.offsetLeft;
+    const marioPosition =  +window.getComputedStyle(mario).bottom.replace('px','');
+     
+    console.log (marioPosition);
+
+    if (pipePosition  <= 80 && pipePosition > 0 && marioPosition < 80) {
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+        
+        pipe.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src= './imagens/mario-dead-stoped.png';
+        mario.style.width='50px'
+        mario.style.marginLeft='50px'
+
+        clearInterval(loop)
+    }
+    
+}, 10);
 
 document.addEventListener('keydown', jump);
